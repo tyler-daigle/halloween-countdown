86be6b5d38d862425f4b6d018ac7eebde95dca79
Tyler D <tyler@tdaigle.com>
2021-06-18 15:48:01 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 80;
+const port =  process.env.PORT || 8080;
 
 const numberDaysTillHalloween = require("./halloween");
 
